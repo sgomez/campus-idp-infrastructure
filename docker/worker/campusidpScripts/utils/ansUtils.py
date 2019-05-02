8ddd59b29cc602bbc2f6bc9dfd1d81ad5ba54d96
Sergio Gómez <sergio@uco.es>
2019-05-02 14:49:16 +0200

    Updated worker and aux scripts
    * Added executable bit to ansible script
    * Fixed contacts access in yaml dictionary
    * Java and ansible dependencies
    * CS

--- docker/worker/campusidpScripts/utils/ansUtils.py
+++ docker/worker/campusidpScripts/utils/ansUtils.py
@@ -7,16 +7,20 @@ import utils
 import yaml
 import logging
 
+
 def prepare_dyn_inv(dict_yaml, dict_idp):
-   # Create the jinja2 environment.
-   # Notice the use of trim_blocks, which greatly helps control whitespace.
-   j2_env = Environment(loader=FileSystemLoader(os.getcwd()),keep_trailing_newline=True,trim_blocks=True,lstrip_blocks=True)
-   result = j2_env.get_template('templates/dyn-inv.py.j2').render(yaml_vals=dict_yaml,idp_vals=dict_idp)
-   return result
+    # Create the jinja2 environment.
+    # Notice the use of trim_blocks, which greatly helps control whitespace.
+    j2_env = Environment(loader=FileSystemLoader(
+        os.getcwd()), keep_trailing_newline=True, trim_blocks=True, lstrip_blocks=True)
+    result = j2_env.get_template(
+        'templates/dyn-inv.py.j2').render(yaml_vals=dict_yaml, idp_vals=dict_idp)
+    return result
+
 
 def create_dyn_inv(dict_yaml, dict_idp, dest):
-   idp_yml = open("%s/dyn-inv.py" % dest, "w")
-   values = prepare_dyn_inv(dict_yaml, dict_idp)
-   idp_yml.write(values)
-   idp_yml.close()
-
+    idp_yml = open("%s/dyn-inv.py" % dest, "w")
+    values = prepare_dyn_inv(dict_yaml, dict_idp)
+    idp_yml.write(values)
+    idp_yml.close()
+    os.chmod("%s/dyn-inv.py" % dest, 0o744)
